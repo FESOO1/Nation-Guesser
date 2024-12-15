682ae724d3a1ff788048cd11e8ad0560e9f163f8
FES001 <firdavs7794948@gmail.com>
2024-12-15 15:01:11 +0500

    Working on it.

--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,3 +1,17 @@
 const modesButton = document.querySelector('#modesButton');
+const modesContainer = document.querySelector('.nation-guesser-main-menu-itself-mode-button');
+let isModesContainerActivated = false;
 
 // CHANGING THE MODES
+
+function activatingTheModesContainer() {
+    if (isModesContainerActivated === false) {
+        modesContainer.classList.add('nation-guesser-main-menu-itself-mode-button-active');
+        
+        isModesContainerActivated = true;
+    } else {
+        modesContainer.classList.remove('nation-guesser-main-menu-itself-mode-button-active');
+        
+        isModesContainerActivated = false;
+    };
+};
